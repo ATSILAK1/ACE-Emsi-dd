fc5f8abd1369266e03222e0d75397e6c8126ece7
USER <USER@DESKTOP-EGDNFSG>
2023-10-27 20:37:43 +0100

    Changement effectue

---
+++ src/main/java/com/lmadani/tpcustomerapplication/jsf/CustomerBean.java
@@ -0,0 +1,43 @@
+package com.lmadani.tpcustomerapplication.jsf;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+
+
+import com.lmadani.tpcustomerapplication.service.CustomerManager;
+import com.lmadani.tpcustomerapplication.model.Customer;
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ *
+ * @author USER
+ */
+@Named(value = "customerBean")
+@ViewScoped
+public class CustomerBean implements Serializable{
+    private List<Customer> customerList ;
+    
+    @Inject
+    private CustomerManager customerManager;
+    /**
+     * Creates a new instance of CustomerBean
+     */
+    public CustomerBean() {
+      
+       }
+    
+    public List<Customer> getCustomers()
+    {
+          if (customerList == null )
+            customerList = customerManager.getAllCustomers();
+        
+        return customerList ;
+    }
+    
+}
